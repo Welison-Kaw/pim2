44db1aca17323e5a0fa8c762405459d501019e37
Welison-Kaw <welisoncomw@gmail.com>
2017-10-16 20:37:04 -0200

    Alterações em sala
    Criação inicial da tela de cadastro de Cliente;
    Criação inicial da tela de visualizar Cliente;
    Alteração no menu;

--- pim2.cpp
+++ pim2.cpp
@@ -37,13 +37,13 @@ void backcolor (DOS_COLORS iColor) {
         SetConsoleTextAttribute (hl, bufferInfo.wAttributes |= (iColor << 4));
 }
 
-centraliza(int l, std::string s) {
+void centraliza(int l, std::string s) {
 	//gotoxy((120-s.length())/2,l);
 	gotoxy((80-s.length())/2,l);
 	std::cout << s;
 }
 
-borda (int xa, int ya, int xb, int yb, int tipo) {
+void borda (int xa, int ya, int xb, int yb, int tipo) {
 	int bordaSimples[6] = {218,191,192,217,196,179};
 	int bordaDupla[6] = {201,187,200,188,205,186};
 	int bordaTipo[6];
@@ -78,22 +78,212 @@ borda (int xa, int ya, int xb, int yb, int tipo) {
 	}
 }
 
-titulo() {
+bool valida_entrada(std::string n) {
+	/*if (n.length() > 0) {
+		return true;
+	} else {
+		return false;
+	}*/
+	
+	return true;
+}
+
+void titulo() {
 	centraliza(tituloLinha, tituloTexto);
 	//borda((120-tituloTexto.length())/2-2,tituloLinha-1,(120-tituloTexto.length())/2+tituloTexto.length()+1,tituloLinha+1,1);
 	borda((80-tituloTexto.length())/2-2,tituloLinha-1,(80-tituloTexto.length())/2+tituloTexto.length()+1,tituloLinha+1,1);
 }
 
-tela (int v) {
+/*void tela (int v) {
 	system("cls");
 	
 	gotoxy (57,2);
 
 	std::cout << "Tela " << v+1;
 	getch();
+}*/
+
+void telaCadastroClientes() {
+	system("cls");
+	int l = 7;
+	bool salvar;
+	
+	char nome[30];
+	char rg[9];
+	char cpf[14];
+	char habilitacao[10];
+	char nascimento[10];
+	char ong[10];
+	
+	textcolor(WHITE);
+	titulo();
+	borda(0,0,79,24,2);
+	
+	centraliza(l-2, "Cadastro de Clientes");
+	
+	borda(2,l-1,77,23,1);
+
+	std::string item_cad[6] = {"Nome/Razao Social:", "RG:", "CPF/CNPJ:", "Habilitacao:", "Nascimento:", "ONG:"};
+	
+	textcolor(LIGHT_RED);
+	for (int i=0; i<6; i++) {
+		gotoxy(5,++l);
+		++l;
+		cout << item_cad[i];
+	}
+	
+	l = 7;
+
+	textcolor(WHITE);
+	gotoxy(item_cad[0].length()+6, ++l);
+
+	/*   ~~~~~~~~~ TESTE OS DOIS ~~~~~~~~  */	
+	/*nome = valida_entrada(nome);
+	
+	while (!valida_entrada(nome)) {
+		scanf("%s", &nome);
+	}*/
+	
+	++l;
+	scanf("%s", &nome);
+	gotoxy(item_cad[1].length()+6, ++l);
+	++l;
+	scanf("%s", &rg);
+	gotoxy(item_cad[2].length()+6, ++l);
+	++l;
+	scanf("%s", &cpf);
+	gotoxy(item_cad[3].length()+6, ++l);
+	++l;
+	scanf("%s", &habilitacao);
+	gotoxy(item_cad[4].length()+6, ++l);
+	++l;
+	scanf("%s", &nascimento);
+	gotoxy(item_cad[5].length()+6, ++l);
+	++l;
+	scanf("%s", &ong);
+		
+	l++;	
+	//borda(20,l,60,l+4,1);
+	
+	centraliza(l++, "Deseja salvar o usuario?");
+	//centraliza(++l, "Sim   Nao");
+
+	gotoxy(35, l);
+	cout << "Sim";
+	gotoxy(41, l);
+	textcolor(LIGHT_RED);
+	cout << "Nao";
+	salvar = false;
+
+	unsigned char cIn;
+	do {
+		cIn = getch();
+		switch ((int)cIn) {
+			case 75:
+				textcolor(LIGHT_RED);
+				gotoxy(35,l);
+				cout << "Sim";
+				textcolor(WHITE);
+				gotoxy(41,l);
+				cout << "Nao";
+				salvar = true;
+				break;
+			case 77:
+				textcolor(WHITE);
+				gotoxy(35,l);
+				cout << "Sim";
+				textcolor(LIGHT_RED);
+				gotoxy(41,l);
+				cout << "Nao";
+				salvar = false;
+				break;
+		}
+	} while ((int)cIn != 13);
+	
+	if (salvar) {
+		/* SALVAR ARQUIVO */
+	}
 }
 
-telaListaVeiculos() {
+void telaListaClientes() {
+	system("cls");
+	
+	string linha;
+	ifstream arquivoCliente("clientes.dat");
+	int posicao = 0;
+	int tamanhoColuna = 12;
+	int l;
+	
+	textcolor(WHITE);
+	titulo();
+	borda(0,0,79,24,2);
+	l = 7;
+	
+	/*l=7;
+	centraliza(l-2, "Cadastro de Clientes");*/
+	
+	if (arquivoCliente.is_open()) {
+		l = 7;
+		centraliza(l-2,"Lista de Clientes");
+		borda(2,l-1,77,23,1);
+		gotoxy(5,l);
+		
+		while (getline(arquivoCliente, linha)) {
+			
+			switch (linha[0]) {
+				case '0':
+					textcolor(WHITE);
+					break;
+				case '1':
+					textcolor(LIGHT_RED);
+					break;
+				case 'F':
+					textcolor(LIGHT_GREEN);
+					break;
+			}
+
+			for(std::string::size_type i = 0; i < linha.size(); ++i) {
+				if (posicao > 0 && linha[i] != ';') {
+					cout << linha[i];
+				}
+				if (posicao > 1 && linha[i] == ';') {
+					for (int i=posicao;i<=tamanhoColuna;i++) {
+						cout << ' ';
+					}
+					posicao = 1;
+				}
+				posicao++;
+			}
+			
+			l++;
+			gotoxy(5,l);
+			
+			posicao = 0;
+		}
+		arquivoCliente.close();
+	} else {
+		cout << "Não foi possível abrir o arquivo";
+	}
+	
+	getch();
+}
+
+void telaCadastroVeiculos() {
+	system("cls");
+	int l = 7;
+
+	textcolor(WHITE);
+	titulo();
+    borda(0,0,79,24,2);
+    
+    borda(2,l-1,77,23,1);
+    
+	centraliza(l-2,"Cadastro de Veiculos");
+
+	getch();
+}
+
+void telaListaVeiculos() {
 	system("cls");
 	string linha;
 	ifstream arquivoVeiculos("veiculos.dat");
@@ -146,17 +336,17 @@ telaListaVeiculos() {
 		}
 		arquivoVeiculos.close();
 	} else {
-		cout << "N�o foi poss�vel abrir o arquivo";
+		cout << "Não foi possível abrir o arquivo";
 	}
 	
 	getch();
 }
 
-tela_menu(int menu) {
+void tela_menu(int menu) {
 	//clrscr();
 	system("cls");
 	int linha;
-	string item_menu[5] = {"Cadastrar Novo Cliente", "Visualizar Lista de Clientes", "Cadastrar Novo Veiculo", "Verificar Lista de Veiculos", "Sair"};
+	string item_menu[6] = {"Cadastrar Novo Cliente", "Visualizar Lista de Clientes", "Cadastrar Novo Veiculo", "Verificar Lista de Veiculos", "Devolucao", "Sair"};
 	
 	// BRANCO
 	textcolor(WHITE);
@@ -166,7 +356,7 @@ tela_menu(int menu) {
 	
 	linha = 6;
 	
-	for (int i=0; i<5; i++) {
+	for (int i=0; i<6; i++) {
 		if (menu == i) {
 			textcolor(LIGHT_RED);
 		} else {
@@ -184,7 +374,7 @@ tela_menu(int menu) {
 				textcolor(WHITE);
 				centraliza(8+menu*2, item_menu[menu]);
 				if (menu == 0) {
-					menu = 4;
+					menu = 5;
 				} else {
 					menu--;
 				}
@@ -194,7 +384,7 @@ tela_menu(int menu) {
 			case 80:
 				textcolor(WHITE);
 				centraliza(8+menu*2, item_menu[menu]);
-				if (menu == 4) {
+				if (menu == 5) {
 					menu = 0;
 				} else {
 					menu++;
@@ -206,23 +396,36 @@ tela_menu(int menu) {
 	} while ((int)cIn != 13);
 	
 	switch (menu) {
+		case 0:
+			telaCadastroClientes();
+			tela_menu(menu);
+			break;
+		case 1:
+			telaListaClientes();
+			tela_menu(menu);
+			break;
+		case 2:
+			telaCadastroVeiculos();
+			tela_menu(menu);
+			break;
 		case 3:
 			telaListaVeiculos();
 			tela_menu(menu);
 			break;
 		case 4:
-			break;
-		default:
-			tela(menu);
+			//
 			tela_menu(menu);
+			break;
+		case 5:
+			break;
 	}
 }
 
 int main() {
-	int menu = 4;
+	//int menu = 4;
+	int menu = 0;
     tela_menu(menu);
     //getch();
 
 	return 0;
 }
-
